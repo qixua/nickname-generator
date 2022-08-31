caeafdcc0dfaa08c68b6fdeb2c7e6e981d6673c8
qixua <qixua@proton.me>
2022-08-31 16:23:44 +0300

    refactor: give red color to error

--- utils/handle_value_error.py
+++ utils/handle_value_error.py
@@ -1,6 +1,8 @@
 def handle_value_error(*, value_to_check, test_fn, message):
+    red = "\033[1;31m"
+
     try:
         return test_fn(value_to_check)
     except ValueError:
-        print(message)
+        print(f"{red}{message}")
         exit()
